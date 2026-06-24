b1e6da1f6f1cbb848dbdc0660b76f78c5c735db4
Pashuwala agrotech <pashuwala.agrotech@yahoo.com>
2026-06-24 12:00:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // IMPORTANT: Replace 'brand-zsharad' with your repo name
+  basePath: process.env.NODE_ENV === 'production' ? '/brand-zsharad' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/brand-zsharad/' : '',
+};
 
+module.exports = nextConfig;
